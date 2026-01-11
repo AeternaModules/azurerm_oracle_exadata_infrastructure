ada05e6300de571f5c68f6610373ee7214d859ff
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:57:14 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "oracle_exadata_infrastructures" {
+  description = "All oracle_exadata_infrastructure resources"
+  value       = azurerm_oracle_exadata_infrastructure.oracle_exadata_infrastructures
+}
+
